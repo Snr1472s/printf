0aeee9c794a1022d292446764034fb749b21f25d
Snr1472s <136021542+Snr1472s@users.noreply.github.com>
2023-08-24 11:25:18 +0100

    printf_main.h

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <limits.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
 
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
@@ -71,17 +71,15 @@ typedef struct specifier
 int _puts(char *str);
 int _putchar(int c);
 
+/* string_fields.c modoule */
+char *get_precision(char *p, params_t *params, va_list ap);
+
 /* print_functions.c module */
 int print_char(va_list ap, params_t *params);
 int print_int(va_list ap, params_t *params);
 int print_string(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
 int print_S(va_list ap, params_t *params);
-
-/* number.c module */
-char *convert(long int num, int base, int flags, params_t *params);
-int print_unsigned(va_list ap, params_t *params);
-int print_address(va_list ap, params_t *params);
 
 /* specifier.c module */
 int (*get_specifier(char *s))(va_list ap, params_t *params);
@@ -96,11 +94,6 @@ int print_HEX(va_list ap, params_t *params);
 int print_binary(va_list ap, params_t *params);
 int print_octal(va_list ap, params_t *params);
 
-/* simple_printers.c module */
-int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, params_t *params);
-int print_rot13(va_list ap, params_t *params);
-
 /* print_number.c module */
 int _isdigit(int c);
 int _strlen(char *s);
@@ -108,13 +101,20 @@ int print_number(char *str, params_t *params);
 int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
 
+/* simple_printers.c module */
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
 /* params.c module */
 void init_params(params_t *params, va_list ap);
 
-/* string_fields.c modoule */
-char *get_precision(char *p, params_t *params, va_list ap);
-
 /* _prinf.c module */
 int _printf(const char *format, ...);
+
+/* number.c module */
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t *params);
 
 #endif
